27f31d47fc2e6ade6356fee56bda127491dcf3eb
manhunter7-2 <thomas.piquet7@gmail.com>
2023-05-11 23:56:33 +0200

    cleaning server + swagger_output deletion

--- server.js
+++ server.js
@@ -1,25 +1,6 @@
 const express = require('express')
-// bodyParser = require("body-parser"),
-// const swaggerAutogen = require("swagger-autogen")
-// const swaggerUi = require("swagger-ui-express");
-// const swaggerFile = require('./swagger_output.json')
-// const outputFile = "./swagger-output.json"
-// const endpointsFiles = ["./controller/index"]
-// const config = {}
 const app = express()
 const port = 3000
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.get('/ez', (req, res) => {
-//     res.send('izi pizi')
-//   })
-
-// swaggerAutogen()(outputFile, endpointsFiles, config).then(async () => {
-//   await import('./index.js'); // Your express api project's root file where the server starts
-// });
 
 app.use('/', require('./routes/routes'))
 
