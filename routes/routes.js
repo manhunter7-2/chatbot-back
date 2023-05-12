4fb5b02274c56c286ac587282c3235b388621fa7
manhunter7-2 <thomas.piquet7@gmail.com>
2023-05-12 08:22:37 +0200

    swagger OK

--- routes/routes.js
+++ routes/routes.js
@@ -3,12 +3,59 @@ const router = express.Router()
 
 const controllers = require('../cotroller/index')
 
+
+
+/**
+ * @swagger
+ * /api/v1/:
+ *  get:
+ *    description: Home route
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.get('/', controllers.home)
 
+/**
+ * @swagger
+ * /api/v1/test:
+ *  get:
+ *    description: Test route
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.get('/test', controllers.test)
 
+/**
+ * @swagger
+ * /api/v1/dialog/questions:
+ *  get:
+ *    description: Use to request all questions
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.get('/dialog/questions', controllers.findAllQuestions)
 
+/**
+ * @swagger
+ * /api/v1/dialog/answer/{id}:
+ *  get:
+ *    description: Use to find dialog by id
+ *    parameters:
+ *      - name: id
+ *        in: path
+ *        description: ID of the dialog
+ *        required: true
+ *        schema:
+ *          type: integer
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ *      '404':
+ *        description: Dialog not found
+ */
 router.get('/dialog/answer/:id', controllers.findById)
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,13 +2,32 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use('/', require('./routes/routes'))
+// Swagger
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Dialog API',
+            description: 'Dialog API Information',
+            contact: {
+                name: 'Developer Name',
+            },
+            servers: ['http://localhost:3000'],
+        },
+    },
+    apis: ['./routes/routes.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get('*', (req, res) => {
-  // res.status(404).json({message: 'Not found'}) bonne pratique
-  res.sendFile(__dirname + '/view/404.html')
+    // res.status(404).json({message: 'Not found'}) bonne pratique
+    res.sendFile(__dirname + '/view/404.html')
 })
-  
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Listening on port ${port}`)
 })
